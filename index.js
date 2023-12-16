c7bc105bab1a48e810f242c51a60c34d35ea8550
Besarta Mustafa <besarta@Besartas-MacBook-Air.local>
2023-12-17 00:33:09 +0100

    update

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const jsonServer = require('json-server')
+const cors = require('cors')
+const path = require('path')
+
+const server = jsonServer.create()
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const middlewares = jsonServer.defaults()
+
+server.use(cors())
+server.use(jsonServer.bodyParser)
+server.use(middlewares)
+server.use(router)
+
+const PORT = 3000
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`)
+})
